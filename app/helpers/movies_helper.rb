1d20474c76d964bcd6e807dbd292eb42d7596145
saasbook <saasbook@saasbook.(none)>
2013-02-17 13:39:06 -0800

    Third

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,7 +3,9 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-	def sortable(column)
+	def sortable(column, title = nil)
+		title ||= column.titleize
 		hilite = column == sort_column ? "current"
+		link_to title, :sort => column
 	end	
 end
