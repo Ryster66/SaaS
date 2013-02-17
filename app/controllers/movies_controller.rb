905c4bbe7d655a8ac6a4f673325fb3984e32b8c2
saasbook <saasbook@saasbook.(none)>
2013-02-17 12:37:07 -0800

    Third

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-		Movie.find(:all, :order => 'title')
+		
   end
 
   def index
@@ -23,6 +23,7 @@ class MoviesController < ApplicationController
 
   def edit
     @movie = Movie.find params[:id]
+		Movie.find(:all, :order => 'title')
   end
 
   def update
